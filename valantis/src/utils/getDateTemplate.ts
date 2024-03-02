f2291b4b716324ed7112c38e1505919a7689efbc
codestudent24 <belgorod2407@yandex.ru>
2024-03-03 00:35:47 +1000

    fix: add timezone offset for MSK

--- valantis/src/utils/getDateTemplate.ts
+++ valantis/src/utils/getDateTemplate.ts
@@ -1,5 +1,6 @@
 export function getDateTemplate(): string {
   const date = new Date()
+  date.setTime(date.getTime() - 3 * 60 * 60 * 1000)
   const year = date.getFullYear();
   const month = `${(date.getMonth() + 1)}`.padStart(2, '0')
   const day = `${date.getDate()}`.padStart(2, '0')
